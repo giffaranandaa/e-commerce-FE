5c58642447e41d4fbdb49d23b833c31159da6603
gipparananda <giffaranandaa@gmail.com>
2024-02-04 22:32:30 +0700

    setup deppendencies

---
+++ src/app/page.js
@@ -0,0 +1,7 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div className="h-full flex items-center justify-center text-center">Gamming GammingBrozz</div>
+  );
+}
